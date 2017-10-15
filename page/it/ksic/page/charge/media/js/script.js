98f382840dd83910f7aeb64a024b91e8f2c78c86
NickSettler <moiseevnikita14@gmail.com>
2017-10-15 15:45:18 +0500

    all cleared

--- page/it/ksic/page/charge/media/js/script.js
+++
@@ -1,9 +0,0 @@
-function playVideo(){
-	document.getElementsByClassName("myVideo")[0].style.top = "0";
-    document.body.style.overflow = "hidden";
-}
-
-function closeVideo(){
-    document.getElementsByClassName("myVideo")[0].style.top = "-100vh";
-    document.body.style.overflow = "auto";
-}
